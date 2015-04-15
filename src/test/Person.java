f2a0e508f172b2c49587113a308322c238bbd6e8
Lasse Ørberg <hejlasse@gmail.com>
2015-04-15 09:17:59 +0200

    First test-commit

---
+++ src/test/Person.java
@@ -0,0 +1,39 @@
+package test;
+
+/**
+ * A person with name, address, tel and email
+ * Must be linked with the company class
+ * and can be linked with attendance class
+ */
+public class Person {
+	/**
+	 * First and last name of person
+	 */
+	private String navn;
+	
+	/**
+	 * Full address of person
+	 */
+	private String address;
+	
+	/**
+	 * Telephone number of person
+	 */
+	private int telephone;
+	
+	/**
+	 * e-mail address of person
+	 */
+	private String email;
+	
+	/**
+	 * Company person works for (company class)
+	 */
+	// Implement
+	
+	/**
+	 * One or more attendances of person implemented as a class (attendance) 
+	 */
+	// Implement
+
+}
