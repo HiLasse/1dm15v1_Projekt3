54a527c53a836e93822b9f6e096de42b1fdac156
Kristian <Kristian@10.10.131.248>
2015-04-29 10:53:06 +0200

    Hotel

---
+++ src/model/Hotel.java
@@ -0,0 +1,32 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Hotel 
+{	
+	private String name;
+	private double pricePrDay;
+
+	// link to Ekstraydelser class (--> 0..*)
+	private ArrayList<HotelService> services;
+	
+	
+	private ArrayList<HotelService> hotelServices;
+//github.com/HiLasse/1dm15v1_Projekt3.git
+	public Hotel()
+	{
+
+	}
+
+	public double calcPrice()
+	{
+
+		return 0.0;
+	}
+
+	public double getpricePrDay()
+	{
+		return pricePrDay;
+	}
+
+}
