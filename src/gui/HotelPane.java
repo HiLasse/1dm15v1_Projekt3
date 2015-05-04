ef48548b63c6dd205fd54418a8584daa46aa3abd
kristian <kristian@10.10.131.118>
2015-05-04 11:18:56 +0200

    Finishing gui classes fundamentals

---
+++ src/gui/HotelPane.java
@@ -0,0 +1,18 @@
+package gui;
+
+import javafx.beans.value.ChangeListener;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class HotelPane extends GridPane{
+
+}
