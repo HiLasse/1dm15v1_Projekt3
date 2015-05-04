1fe198a8f537185dc2784604fb66bcc0c7d5dab0
Kristian <Kristian@10.10.131.139>
2015-05-04 08:44:39 +0200

    Service

--- src/service/Service.java
+++ src/service/Service.java
@@ -9,103 +9,93 @@ import model.Participant;
 import model.Registration;
 import storage.Storage;
 
-public class Service {	
+public class Service { 
 	//Company methods
-	
+
 	public static Company createCompany(String name, String address, int telephone) {
 		Company company = new Company(name, address, telephone);
 		Storage.addCompany(company);
 		return company;
 	}
-	
+
 	public static void updateCompany(Company company, String name, String address, int telephone) {
 		company.setAddress(address);
 		company.setName(name);
 		company.setTelephone(telephone);
 	}
-	
+
 	public static void deleteCompany(Company company) {
 		Storage.removeCompany(company);
 	}
 	//-----------------------------------------------------------------------------------------
 
-	//Participent methods
-	
+	//Participant methods
+
 	public static Participant createParticipant(String name, String address, int telephone, String email) {
 		Participant participant = new Participant(name, address, telephone, email);
 		Storage.addParticipent(participant);
 		return participant;
 	}
-	
+
 	public static void updateParticipant(Participant participant, String name, String address, int telephone, String email) {
 		participant.setAddress(address);
 		participant.setEmail(email);
 		participant.setName(name);
 		participant.setTelephone(telephone);
 	}
-	
+
 	public static void deleteParticipant(Participant participant) {
 		Storage.removeParticipent(participant);
 	}
-	
+
 	//-----------------------------------------------------------------------------------------
-	
+
 	//Hotels methods
-	
+
 	public static Hotel createHotel(String name, double pricePrDay) {
 		Hotel hotel = new Hotel(name, pricePrDay);
 		Storage.addHotel(hotel);
 		return hotel;
 	}
-	
+
 	public static void updateHotel(Hotel hotel, String name, double pricePrDay) {
 		hotel.setName(name);
 		hotel.setPricePrDay(pricePrDay);
 	}
-	
+
 	public static void deleteHotel(Hotel hotel) {
 		Storage.removeHotel(hotel);
 	}
-	
-	
+
+
 	//-----------------------------------------------------------------------------------------
-	
+
 	//Conference methods
-	
+
 	public static Conference createConference(String name, String address, LocalDateTime startTime, LocalDateTime endTime, double price) {
 		Conference conference = new Conference(name, address, startTime, endTime, price);
 		Storage.addConference(conference);
 		return conference;
 	}
-	
+
 	public static void updateConference(Conference conference, String name, String address, LocalDateTime startTime, LocalDateTime endTime) {
 		conference.setName(name);
 		conference.setAddress(address);
 		conference.setEndTime(endTime);
 		conference.setStartTime(startTime);
 	}
-	
+
 	public static void deleteConference(Conference conference) {
 		Storage.removeConference(conference);
 	}
-	
+
 	//----------------------------------------------------------------------------------------
-	
+
 	//Registration methods
-	
-	public static Registration createRegistration(Conference conference, Participant participant, LocalDateTime arrivalDate,LocalDateTime departureDate) {
+
+	public static Registration createRegistration() {
 		//TODO: add method
-		Registration restration = new Registration(conference, participant, arrivalDate, departureDate); //  conference, participant, arrivaldate, departuredate
-		Storage.addRegistration(restration);
-		return restration;
+		return null;
 	}
-	
-	public static void updateRegistration() {
-		
-	}
-	
-	public static void removeRegistration() {
-		//TODO: add method
-	}
-	
+
 }
