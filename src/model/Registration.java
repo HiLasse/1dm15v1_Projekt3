2addd15431313192283b733435f3d6312f892ef5
Erik <Erik@10.10.138.139>
2015-05-04 09:38:45 +0200

    Pris metode

--- src/model/Registration.java
+++ src/model/Registration.java
@@ -6,23 +6,25 @@ import java.util.ArrayList;
 
 public class Registration 
 {
-	
-	
+
+
 	private Companion companion;
+	private Participant participant;
 	private Hotel hotel;
 	private Conference conference;
 	private ArrayList<Excursion> excursions = new ArrayList<>();
 	private ArrayList<HotelService> hotelServices = new ArrayList<>();
 	private LocalDateTime arrivalDate;
 	private LocalDateTime departureDate;
-	
+
 	public Registration(Conference conference,Participant participant, LocalDateTime arrivalDate,LocalDateTime departureDate)
 	{
 		this.conference = conference;
 		this.arrivalDate = arrivalDate;
 		this.departureDate = departureDate;
+		this.participant = participant;
 	}
-	
+
 	//With companion
 	public Registration(Conference conference,Participant participant, LocalDateTime arrivalDate,LocalDateTime departureDate, Companion companion)
 	{
@@ -31,8 +33,8 @@ public class Registration
 		this.departureDate = departureDate;
 		this.companion = companion;
 	}
-	
-	
+
+
 	// beregner pris for alt (service, hotel, konference)
 	public double calcTotalPrice()
 	{
@@ -44,80 +46,84 @@ public class Registration
 			{
 				totalPrice += x.getPrice();
 			}
-			
+
 			// Antal dage på hotel 
 			totalPrice += hotel.getPricePrDay()*days();
-			
-			// Konference pris
-			totalPrice += conference.getPrice();
-			
-			// Udflugt pris
-			for (Excursion excursion : excursions) 
-			{
-				totalPrice += excursion.getPrice();
-			}
-			
-			//TODO: Checkup
 		}
+		// Konference pris
+		totalPrice += conference.getPrice();
+
+		// Udflugt pris
+		for (Excursion excursion : excursions) 
+		{
+			totalPrice += excursion.getPrice();
+		}
+		
+		if( participant.isLecture())
+		{
+			totalPrice = 0;
+		}
+		//TODO: Checkup
 		return totalPrice;
 	}
-	
-	
-	/**
-	 * @return days for the registration
-	 */
-	private long days()
-	{
-		return ChronoUnit.DAYS.between(arrivalDate, departureDate);
-	}
 
-	
-	// Getters and setters
-	public Companion getCompanion() {
-		return companion;
-	}
 
-	public void setCompanion(Companion companion) {
-		this.companion = companion;
-	}
 
-	public Hotel getHotel() {
-		return hotel;
-	}
+/**
+ * @return days for the registration
+ */
+private long days()
+{
+	return ChronoUnit.DAYS.between(arrivalDate, departureDate);
+}
 
-	public void setHotel(Hotel hotel) {
-		this.hotel = hotel;
-	}
 
-	public Conference getConference() {
-		return conference;
-	}
+// Getters and setters
+public Companion getCompanion() {
+	return companion;
+}
 
-	public void setConference(Conference conference) {
-		this.conference = conference;
-	}
+public void setCompanion(Companion companion) {
+	this.companion = companion;
+}
 
-	public ArrayList<Excursion> getExcursions() {
-		return excursions;
-	}
+public Hotel getHotel() {
+	return hotel;
+}
 
-	public LocalDateTime getArrivalDate() {
-		return arrivalDate;
-	}
+public void setHotel(Hotel hotel) {
+	this.hotel = hotel;
+}
 
-	public void setArrivalDate(LocalDateTime arrivalDate) {
-		this.arrivalDate = arrivalDate;
-	}
+public Conference getConference() {
+	return conference;
+}
 
-	public LocalDateTime getDepartureDate() {
-		return departureDate;
-	}
+public void setConference(Conference conference) {
+	this.conference = conference;
+}
 
-	public void setDepartureDate(LocalDateTime departureDate) {
-		this.departureDate = departureDate;
-	}
-	
-	
-	
-	
+public ArrayList<Excursion> getExcursions() {
+	return excursions;
+}
+
+public LocalDateTime getArrivalDate() {
+	return arrivalDate;
+}
+
+public void setArrivalDate(LocalDateTime arrivalDate) {
+	this.arrivalDate = arrivalDate;
+}
+
+public LocalDateTime getDepartureDate() {
+	return departureDate;
+}
+
+public void setDepartureDate(LocalDateTime departureDate) {
+	this.departureDate = departureDate;
+}
+
+
+
+
 }
